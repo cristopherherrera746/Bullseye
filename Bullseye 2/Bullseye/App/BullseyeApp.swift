59c03de580a95a05b3d1aca54af8b2e610611085
cristopherherrera746 <72266990+cristopherherrera746@users.noreply.github.com>
2021-01-14 08:46:37 -0600

    Add files via upload

---
+++ Bullseye 2/Bullseye/App/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by A.M. Student on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye 2/Bullseye/Models/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by A.M. Student on 1/6/21.
+//
+
+import Foundation
+
+struct Game {
+    var target = Int.random(in: 1...100)
+    var score = 0
+    var round = 1
+    
+    func points(sliderValue: Int) -> Int {
+        100 - abs(target - sliderValue)
+    }
+}
